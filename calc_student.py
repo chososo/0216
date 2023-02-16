f31203df3f36f593a6f670a607fe9f8898a0fba6
chososo <a50242953@gmail.com>
2023-02-16 11:51:18 +0900

    commit error solved

--- calc_student.py
+++ calc_student.py
@@ -8,7 +8,8 @@ Kebab-Casing'''
 def calcme(student_):
     student_['tot']=student_['kor']+student_['eng']+student_['math']
     student_['avg']=student_['tot']/3
-    student_['grade']=None
+    student_['grade']= None
+    
     if student_['avg']>= 90 :  student_['grade'] ='A'
     elif student_['avg']>= 80 :  student_['grade'] ='B'
     elif student_['avg']>= 70 : student_['grade'] = 'C'
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import util , input_student,calc_student
+import util , input_student,calc_student,output_student
 
 
 student={}
 
 if __name__ == '__main__' :
+    
     input_student.inputme(student)
     calc_student.calcme(student)
-
-    
+    output_student.outputme(student)
+    print('Program is over ... ')
     
