322a54926e0a674b88ccb0ef778e2682df64b3bf
chososo <a50242953@gmail.com>
2023-02-16 11:27:37 +0900

    2차 커밋

---
+++ input_student.py
@@ -0,0 +1,6 @@
+def inutme(student_):
+    student_['name']=input('이름:')
+    student_['age']=int(input('나이:'))
+    student_['kor']=input('국어:')
+    student_['eng']=int(input('영어:'))
+    student_['math']=int(input('수학:'))
